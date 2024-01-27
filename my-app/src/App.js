3c81cb70440f06b4b12fb89b802edf407650ca6f
savisaluwadana <82082138+savisaluwadana@users.noreply.github.com>
2024-01-27 09:27:10 +0530

    Update index.html and App.js files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,7 @@
-
+// App.js
 import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   const [data, setData] = useState('');
@@ -20,7 +22,21 @@ function App() {
 
   return (
     <div className="App">
-      <h1>{data}</h1>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <h1>{data}</h1>
+      </header>
     </div>
   );
 }
