e3b728910a8c1b1139b5aacfe98f917c6337ca2e
savisaluwadana <82082138+savisaluwadana@users.noreply.github.com>
2024-01-27 09:22:55 +0530

    Add new files and update configurations

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:5000');
+      const result = await response.text();
+      setData(result);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{data}</h1>
     </div>
   );
 }
