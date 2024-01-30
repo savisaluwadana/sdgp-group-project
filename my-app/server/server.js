058531647f51e1efbf73fdbb224e95feea66111d
savisaluwadana <82082138+savisaluwadana@users.noreply.github.com>
2024-01-30 11:19:09 +0530

    Update server.js to include backend and front end connection

--- my-app/server/server.js
+++ my-app/server/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Hello from the server!');
+  res.send('Hello from the server! backend front end connected');
 });
 
 app.listen(PORT, () => {
